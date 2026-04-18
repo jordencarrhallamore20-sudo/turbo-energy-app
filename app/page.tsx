cd17582a1c3ff0d9be1e854e3c7039659bb88540
jordencarrhallamore20-sudo <jordencarrhallamore20@gmail.com>
2026-04-18 10:59:34 +0200

    Update page.tsx
    update page

--- app/page.tsx
+++ app/page.tsx
@@ -1,8 +1,8 @@
 export default function Page() {
   return (
     <div style={{ padding: 40 }}>
-      <h1>Turbo Energy App is LIVE 🚀</h1>
-      <p>If you see this, deployment is working.</p>
+      <h1>Turbo Energy App</h1>
+      <p>New build is working ✅</p>
     </div>
   );
 }
