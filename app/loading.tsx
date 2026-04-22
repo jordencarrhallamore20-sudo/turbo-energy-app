60fcb2605284c2333012b2d42dbebc789f3a58c7
jordencarrhallamore20-sudo <jordencarrhallamore20@gmail.com>
2026-04-22 14:58:22 +0200

    Create loading.tsx
    Fix flicker with loading screen

---
+++ app/loading.tsx
@@ -0,0 +1,17 @@
+export default function Loading() {
+  return (
+    <div style={{
+      height: "100vh",
+      display: "flex",
+      alignItems: "center",
+      justifyContent: "center",
+      background: "#081733",
+      color: "white",
+      fontFamily: "Arial",
+      fontWeight: "800",
+      fontSize: "20px"
+    }}>
+      Loading Turbo Energy Dashboard...
+    </div>
+  );
+}
